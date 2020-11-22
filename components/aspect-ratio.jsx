34a0e3eec1c37b7854cd586383439b55086ada72
Víctor Rico <vricop@gmail.com>
2020-11-22 21:26:20 +0100

    fix: remove wrong class

--- components/aspect-ratio.jsx
+++ components/aspect-ratio.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const AspectRatio = ({ children, className='aspect-ratio-16:9' }) => (
   <figure className="m-0">
-    <div className={`aspect-ratio | overflow-hidden | relative ${className}`}>
+    <div className={`overflow-hidden | relative ${className}`}>
       {children('absolute inset-0 | block | w-100 h-100 | object-cover')}
     </div>
   </figure>
