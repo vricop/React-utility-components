9c1b73d3b0b346ab1919d5761cd5ee82f3b722ea
Víctor Rico <vricop@gmail.com>
2020-11-22 19:39:47 +0100

    refactor: clean up component

--- aspect-ratio.jsx
+++ aspect-ratio.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const AspectRatio = ({ children, fraction=16/9, className, ...restProps }) => (
+const AspectRatio = ({ children, className='aspect-ratio-16:9' }) => (
   <figure className="m-0">
-    <div {...restProps} className={`aspect-ratio | ${className}`} style={{ paddingTop: `calc(100% / ${fraction})` }}>
-      {children}
+    <div className={`aspect-ratio | overflow-hidden | relative ${className}`}>
+      {children('absolute inset-0 | block | w-100 h-100 | object-cover')}
     </div>
   </figure>
 )
 
 AspectRatio.propTypes = {
   children: PropTypes.element.isRequired,
-  fraction: PropTypes.string,
-  className: PropType.string
+  className: PropTypes.string
 };
 
 export default AspectRatio
