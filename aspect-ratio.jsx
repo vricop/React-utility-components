dcc75871107f2f0cd28167e4494af64d42b06e45
Víctor Rico <vricop@gmail.com>
2020-11-22 19:20:22 +0100

    Create aspect-ratio.jsx

---
+++ aspect-ratio.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AspectRatio = ({ children, fraction=16/9, className, ...restProps }) => (
+  <figure style={{ margin:0 }}>
+    <div {...restProps}
+      className={`aspect-ratio | ${className}`}
+      style={{ paddingTop: `calc(100% / ${fraction})` }}>
+      {children}
+    </div>
+  </figure>
+)
+
+AspectRatio.propTypes = {
+  children: PropTypes.element.isRequired,
+  fraction: PropTypes.string
+  className: PropType.string
+};
+
+export default AspectRatio
