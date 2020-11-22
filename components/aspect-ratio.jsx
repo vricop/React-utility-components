a276103d433081632bf2e4a24f9b70295a524c19
Víctor Rico <vricop@gmail.com>
2020-11-22 21:29:44 +0100

    fix: update wrong height and width class utilities

--- components/aspect-ratio.jsx
+++ components/aspect-ratio.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const AspectRatio = ({ children, className='aspect-ratio-16:9' }) => (
   <figure className="m-0">
     <div className={`overflow-hidden | relative ${className}`}>
-      {children('absolute inset-0 | block | w-100 h-100 | object-cover')}
+      {children('absolute inset-0 | block | w-full h-full | object-cover')}
     </div>
   </figure>
 )
