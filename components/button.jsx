d9b1f3d405de757f8cf0e634dd7a6fe39057e385
Víctor Rico <vricop@gmail.com>
2020-11-25 16:33:58 +0100

    refactor: update component for fexible styling
    * Add icon prop
    * Add label prop as an alias for children prop
    * Improve className prop merge
    * Update Proptypes
    * Refactor prop destructuring for readability

--- components/button.jsx
+++ components/button.jsx
@@ -1,31 +1,53 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Icon from '../components/icon'
 
-/*
- * @as: Html elementy type
- * @children: text
- * @background: PetricorCSS `bg-*` utility classes (including variants)
- * @color: PetricorCSS `text-*` utility classes (including variants)
- * @shape: PetricorCSS `rounded-*` utility classes (including variants)
- * @href: URL
- */
-const Button = ({ as='a', children, background='bg-black', color='text-white', shape='rounded', href, ...restProps }) => {
+const Button = (props) => {
+  // Defaults
+  let {
+    as='a',
+    background='bg-info-500 hover:bg-info-700',
+    children,
+    className,
+    color='text-white',
+    href,
+    icon,
+    label,
+    shape='rounded-lg',
+    ...restProps
+  } = props
+
+  // Tag type
   const Tag = href ? 'a' : as
 
+  const presetClassName = `
+    inline-flex items-center | stack-gap-x-3 | font-medium |
+    px-6(em) py-2(em) | leading-8(em) | transition duration-200ms
+  `.trim()
+
+  // Join user defined className props with preset class
+  className = [presetClassName, background, color, shape, className ].join(' | ')
+
   return (
-    <Tag
-      href={as==='a' && href}
-      className={`${background} | ${color} font-medium | inline-block px-6(em) py-2(em) | leading-8(em) | ${shape} | transition duration-200ms`}
-      {...restProps}>
-      {children}
+    <Tag href={as==='a' && href} className={className} {...restProps}>
+      {icon && <Icon name={icon} />} <span>{children || label}</span>
     </Tag>
   )
 }
 
 Button.propTypes = {
   as: PropTypes.string,
-  children: PropTypes.element.isRequired,
-  href: PropTypes.string
+  background: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element
+  ]),
+  className: PropTypes.string,
+  color: PropTypes.string,
+  href: PropTypes.string,
+  icon: PropTypes.string,
+  label: PropTypes.string,
+  shape: PropTypes.string
 };
 
 export default Button
