7233a967a0a5f077db364166df9b90f718776e5d
Víctor Rico <vricop@gmail.com>
2021-03-22 21:36:09 +0100

    Create when.jsx

---
+++ components/when.jsx
@@ -0,0 +1,14 @@
+export default function Show({ a=null, b=null, when, }) {
+  if(condition) return a
+  return b
+}
+
+/*
+  Usage:
+  
+  <Show
+    a={<MyCompenentA />}
+    b={<MyComponentB />}
+    when={true}
+  />
+*/
