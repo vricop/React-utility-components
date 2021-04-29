d2700631cefefbf060b095e511ed0f7671982df5
Víctor Rico <vricop@gmail.com>
2021-04-29 12:51:34 +0200

    Update use-controlled-fields.js

--- hooks/use-controlled-fields.js
+++ hooks/use-controlled-fields.js
@@ -1,23 +1,61 @@
 import { useState } from 'react'
 
 /**
- * Handle fields in controlled forms using state
- * @param {any} initialState - Initial state for the controlled fields element
- * @returns Array(any, function) - Returns the current state, `fields` and a
- * function handler for event `handleFields`, that takes an event object as
- * argument.
+ * Handle fields in controlled form components using state
+ * @param {object} [initialState={}] - Initial state for the controlled fields element
+ * @return Object with methods: {fields, handleFields, handleGroupedFields, setFields}
  */
 export default function useControlledFields(initialState = {}) {
   const [fields, setFields] = useState(initialState)
 
-  const handleFields = ({ currentTarget }) => {
-    const { name, value } = currentTarget
+  /**
+   * Toggles the `checked` attribute from a node element
+   * @param {object} currentTarget An html node
+   * @param {string} type The type of `input` (checkbox, radio...)
+   * @param {bool} checked The booleand value from the `checked` attribute 
+   */
+  const toggleCheckAttribute = ({ currentTarget, type, checked }) => {
+    /*
+      If form currentTarget is either checkbox or radio toggle the checked
+      attribute
+    */
+    if (/(checkbox|radio)/.test(type)) {
+      currentTarget.setAttribute(type, !checked)
+    }
+  }
+
+  /**
+   * Updates the fields `state` from a group of different inputs automatically
+   * when a listener is triggered
+   * @param {object} { currentTarget } The Event object from the listener
+   */
+  const handleGroupedFields = ({ currentTarget }) => {
+    const { name, value, checked, type } = currentTarget
+
+    toggleCheckAttribute({ currentTarget, type, checked })
+
+    // 1. Keep current fields values, if empty default to []
+    const currentFieldsState = [...(fields && fields[name] ? fields[name] : [])]
+    // 2. Use `add` or `delete` method  based on the checked value from the input
+    const toggle = checked ? 'add' : 'delete'
+    // 3. Create a new set based on current fields `state`
+    const fieldSet = new Set([...currentFieldsState])
+    // 4. Add/delete the new value based on 2.
+    fieldSet[toggle](value)
 
     setFields({
       ...fields,
-      [name]: value,
+      [name]: fieldSet,
     })
   }
 
-  return [fields, handleFields, setFields]
+  const handleFields = ({ currentTarget }) => {
+    const { name, value, checked, type } = currentTarget
+
+    toggleCheckAttribute({ currentTarget, type, checked })
+
+    setFields({ ...fields, [name]: value })
+  }
+
+  return { fields, handleFields, handleGroupedFields, setFields }
 }
