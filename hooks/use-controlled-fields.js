ef1a869ffb9abb87e9356cdac23e57df1a1f2bcc
Víctor Rico <vricop@gmail.com>
2021-04-30 12:21:37 +0200

    Update use-controlled-fields.js

--- hooks/use-controlled-fields.js
+++ hooks/use-controlled-fields.js
@@ -9,38 +9,23 @@ export default function useControlledFields(initialState = {}) {
   const [fields, setFields] = useState(initialState)
 
   /**
-   * Toggles the `checked` attribute from a node element
-   * @param {object} currentTarget An html node
-   * @param {string} type The type of `input` (checkbox, radio...)
-   * @param {bool} checked The booleand value from the `checked` attribute 
-   */
-  const toggleCheckAttribute = ({ currentTarget, type, checked }) => {
-    /*
-      If form currentTarget is either checkbox or radio toggle the checked
-      attribute
-    */
-    if (/(checkbox|radio)/.test(type)) {
-      currentTarget.setAttribute(type, !checked)
-    }
-  }
-
-  /**
    * Updates the fields `state` from a group of different inputs automatically
    * when a listener is triggered
    * @param {object} { currentTarget } The Event object from the listener
    */
   const handleGroupedFields = ({ currentTarget }) => {
-    const { name, value, checked, type } = currentTarget
+    const { name, value, checked } = currentTarget
 
-    toggleCheckAttribute({ currentTarget, type, checked })
-
-    // 1. Keep current fields values, if empty default to []
+    /**
+     * 1. Keep a copy of the current field values, if empty default to []
+     * 2. Use `add` or `delete` method  based on the `checked` attribute
+     * 3. Create a new set based on current fields `state`
+     * 4. Add/delete the new value based on the `checked` attribute
+     * 5. Update state
+     */
     const currentFieldsState = [...(fields && fields[name] ? fields[name] : [])]
-    // 2. Use `add` or `delete` method  based on the checked value from the input
     const toggle = checked ? 'add' : 'delete'
-    // 3. Create a new set based on current fields `state`
     const fieldSet = new Set([...currentFieldsState])
-    // 4. Add/delete the new value based on 2.
     fieldSet[toggle](value)
 
     setFields({
@@ -50,10 +35,7 @@ export default function useControlledFields(initialState = {}) {
   }
 
   const handleFields = ({ currentTarget }) => {
-    const { name, value, checked, type } = currentTarget
-
-    toggleCheckAttribute({ currentTarget, type, checked })
-
+    const { name, value } = currentTarget
     setFields({ ...fields, [name]: value })
   }
 
