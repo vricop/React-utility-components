381668cdb12e3d364d4c914d013c012c688ae2eb
Víctor Rico <vricop@gmail.com>
2021-06-10 10:16:54 +0200

    Create use-custom-reducer.js

---
+++ hooks/use-custom-reducer.js
@@ -0,0 +1,33 @@
+import { useReducer } from 'react'
+
+export const ACTIONS = {
+  ACTION_NAME: 'actionName',
+}
+
+const REDUCER_ACTIONS = {
+  [ACTION_NAME]: (state, payload) => ({}),
+}
+
+const reducer = (state, action) => {
+  const reducerAction = REDUCER_ACTIONS[action.type]
+  return reducerAction ? reducerAction(state, action) : state
+}
+
+/**
+ * Custom hook for `useReducer` hook
+ * @param {any} initialState - The initial state
+ * @returns {any} The state and decorated dispatch functions to update the state
+ */
+export default function useFormHook(initialState) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // Functions that decorate the `dispatch` function eg.
+  const updateActionName = () => {
+    dispatch({ type: ACTIONS.ACTION_NAME, payload: 'My data' })
+  }
+
+  return {
+    state,
+    updateActionName,
+  }
+}
